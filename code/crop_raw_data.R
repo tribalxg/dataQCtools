208852961af9cb3f22683ac4e432e989e0c850a3
Jess Kunke <jessicakunke@Jesss-MacBook-Pro.local>
2025-03-12 15:30:56 -0700

    update path to code

--- code/crop_raw_data.R
+++ code/crop_raw_data.R
@@ -21,14 +21,16 @@
 #
 
 # load all the packages and functions you'll need
-source("dataQCfunctions.R")
+source("code/dataQCfunctions.R")
 
 # base path: this is the shared part of the filepath to the directories
 # containing your raw data files, cropped data files, and cropped data plots
 # - if these folders are in the dataQCtools R project folder, you can use a relative path
 # - otherwise you can make this an absolute path
 # - you can also delete base_loc and paste0(base_loc, ...) and just explicitly 
-#   specify rawdata_loc, cropped_loc, and croppedplots_loc
+#   specify rawdata_loc, cropped_loc, and croppedplots_loc (see make_qc_plots.R 
+#   for an example of this)
+# - make sure filepaths for folders always end in "/"!
 base_loc <- "data/2022_spring/"
 
 # directory containing raw data
